290269b46d243f4f26869cc7a8999326d68e21c5
Matt Millar <matthew.millar@madetech.com>
2020-01-17 10:05:14 +0000

    Retrieve stored docs from s3 if they exist

--- api/lib/gateways/S3Gateway.js
+++ api/lib/gateways/S3Gateway.js
@@ -3,31 +3,40 @@ module.exports = function(options) {
 
   return {
     get: async function(id) {
-      // try {
-      //   const doc = await s3
-      //     .getObject({
-      //       Bucket: process.env.S3_BUCKET_NAME,
-      //       Key: `${id}`
-      //     })
-      //     .promise();
-      //   return doc.Body;
-      // } catch (err) {
-      //   console.log(err);
-      // }
+      try {
+        const response = await s3
+          .getObject({
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: `${id}`
+          })
+          .promise();
+        if (response) {
+          console.log(`Got doc id=${id} from s3`);
+          return {
+            doc: response.Body,
+            mimeType: response.Metadata.mimetype
+          };
+        }
+      } catch (err) {
+        if (err.code !== 'NoSuchKey') console.log(err);
+      }
     },
     put: async function(id, document) {
       try {
-        await s3
+        const response = await s3
           .putObject({
             Bucket: process.env.S3_BUCKET_NAME,
             Key: `${id}`,
-            Body: document.doc
+            Body: document.doc,
+            Metadata: { mimetype: document.mimeType }
           })
           .promise();
-        return true;
+        if (response.data) {
+          console.log(`Put doc id=${id} into s3`);
+          return response.data;
+        }
       } catch (err) {
         console.log(err);
-        return false;
       }
     }
   };
